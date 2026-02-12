bc0f6c40b8b9c0d7a17e79922e902be40d1c2213
GK1100 <gaurav30.kumavat@gmail.com>
2026-02-13 04:50:49 +0530

    Integrated mlflow and dagshub & also tested

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,8 +16,14 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-
 from src.utils import save_object,evaluate_models
+
+import mlflow
+import mlflow.sklearn
+import mlflow.catboost
+
+from dotenv import load_dotenv
+load_dotenv()
 
 @dataclass
 class ModelTrainerConfig:
@@ -30,7 +36,16 @@ class ModelTrainer:
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
+            mlflow.set_tracking_uri("https://dagshub.com/GK/ML-Project.mlflow")
+            mlflow.set_experiment("Student_Performance_Experiment")
+            mlflow.sklearn.autolog()
+            mlflow.autolog()
+
+
+            
+            
             logging.info("Split training and test input data")
+            
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -83,36 +98,37 @@ class ModelTrainer:
                 }
                 
             }
+            for model_report in models:
+                with mlflow.start_run():
+                    model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                                    models=models,param=params)
+                    
+                    ## To get best model score from dict
+                    best_model_score = max(sorted(model_report.values()))
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+                    ## To get best model name from dict
+
+                    best_model_name = list(model_report.keys())[
+                        list(model_report.values()).index(best_model_score)
+                    ]
+                    best_model = models[best_model_name]
+
+                    if best_model_score<0.6:
+                        raise CustomException("No best model found")
+                    logging.info(f"Best found model on both training and testing dataset")
+
+                    save_object(
+                        file_path=self.model_trainer_config.trained_model_file_path,
+                        obj=best_model
+                    )
+
+                    predicted=best_model.predict(X_test)
+
+                    r2_square = r2_score(y_test, predicted)
+                    return r2_square
             
-            ## To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
 
-            ## To get best model name from dict
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            best_model = models[best_model_name]
-
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
-
-            save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
-
-            predicted=best_model.predict(X_test)
-
-            r2_square = r2_score(y_test, predicted)
-            return r2_square
-            
-
-
+    
 
             
         except Exception as e:
